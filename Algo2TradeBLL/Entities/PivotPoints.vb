7438b7b1187c55f4789e992dbb07443dfa1ddf79
Indibar <indibar@algo2trade.com>
2019-12-24 10:47:02 +0530

    Before starting new indicator

---
+++ Algo2TradeBLL/Entities/PivotPoints.vb
@@ -0,0 +1,9 @@
+﻿Public Class PivotPoints
+    Public Pivot As Decimal
+    Public Resistance1 As Decimal
+    Public Support1 As Decimal
+    Public Resistance2 As Decimal
+    Public Support2 As Decimal
+    Public Resistance3 As Decimal
+    Public Support3 As Decimal
+End Class
--- Algo2TradeBLL/Indicators/Pivots.vb
+++ Algo2TradeBLL/Indicators/Pivots.vb
@@ -1,33 +1,42 @@
 ﻿Namespace Indicator
     Public Module Pivots
-        Public Enum PivotType
-            Standard = 1
-            Fobonacci
-        End Enum
-        Public Sub CalculatePivots(ByVal pivotType As PivotType,
-                                   ByVal prevHigh As Decimal,
-                                   ByVal prevLow As Decimal,
-                                   ByVal prevClose As Decimal,
-                                   ByRef outputS3 As Decimal,
-                                   ByRef outputS2 As Decimal,
-                                   ByRef outputS1 As Decimal,
-                                   ByRef outputPivot As Decimal,
-                                   ByRef outputR1 As Decimal,
-                                   ByRef outputR2 As Decimal,
-                                   ByRef outputR3 As Decimal
-                                   )
-            Select Case pivotType
-                Case PivotType.Standard
-                    outputPivot = (prevHigh + prevLow + prevClose) / 3
-                    outputS1 = (2 * outputPivot) - prevHigh
-                    outputR1 = (2 * outputPivot) - prevLow
-                    outputS2 = outputPivot - (prevHigh - prevLow)
-                    outputR2 = outputPivot + (prevHigh - prevLow)
-                    outputS3 = outputS2 - (prevHigh - prevLow)
-                    outputR3 = outputR2 + (prevHigh - prevLow)
-                Case Else
-                    Throw New NotImplementedException("Other than standard, no other pivot type is implemented")
-            End Select
+        Public Sub CalculatePivots(ByVal inputPayload As Dictionary(Of Date, Payload), ByRef outputPayload As Dictionary(Of Date, PivotPoints))
+            If inputPayload IsNot Nothing AndAlso inputPayload.Count > 0 Then
+                Dim isFirstCandleOfTheDay As Boolean = False
+                Dim pivotPointsData As PivotPoints = Nothing
+                For Each runningInputPayload In inputPayload
+                    If runningInputPayload.Value.PreviousCandlePayload IsNot Nothing AndAlso
+                        runningInputPayload.Value.PayloadDate.Date <> runningInputPayload.Value.PreviousCandlePayload.PayloadDate.Date Then
+                        isFirstCandleOfTheDay = True
+                        pivotPointsData = New PivotPoints
+                    End If
+                    If isFirstCandleOfTheDay Then
+                        Dim previousDay As Date = runningInputPayload.Value.PreviousCandlePayload.PayloadDate.Date
+                        Dim previousDayPayloads As IEnumerable(Of KeyValuePair(Of Date, Payload)) = inputPayload.Where(Function(x)
+                                                                                                                           Return x.Key.Date = previousDay.Date
+                                                                                                                       End Function)
+                        Dim prevHigh As Decimal = previousDayPayloads.Max(Function(x)
+                                                                              Return x.Value.High
+                                                                          End Function)
+                        Dim prevLow As Decimal = previousDayPayloads.Min(Function(x)
+                                                                             Return x.Value.Low
+                                                                         End Function)
+                        Dim prevClose As Decimal = previousDayPayloads.OrderBy(Function(x)
+                                                                                   Return x.Key
+                                                                               End Function).LastOrDefault.Value.Close
+                        pivotPointsData.Pivot = (prevHigh + prevLow + prevClose) / 3
+                        pivotPointsData.Support1 = (2 * pivotPointsData.Pivot) - prevHigh
+                        pivotPointsData.Resistance1 = (2 * pivotPointsData.Pivot) - prevLow
+                        pivotPointsData.Support2 = pivotPointsData.Pivot - (prevHigh - prevLow)
+                        pivotPointsData.Resistance2 = pivotPointsData.Pivot + (prevHigh - prevLow)
+                        pivotPointsData.Support3 = pivotPointsData.Support2 - (prevHigh - prevLow)
+                        pivotPointsData.Resistance3 = pivotPointsData.Resistance2 + (prevHigh - prevLow)
+                    End If
+                    isFirstCandleOfTheDay = False
+                    If outputPayload Is Nothing Then outputPayload = New Dictionary(Of Date, PivotPoints)
+                    outputPayload.Add(runningInputPayload.Key, pivotPointsData)
+                Next
+            End If
         End Sub
     End Module
 End Namespace
