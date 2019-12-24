c4b8c6ccbfefceeb7a7f835290677aa66a5bd004
Indibar <indibar@algo2trade.com>
2019-12-24 14:42:54 +0530

    After fix

---
+++ Algo2TradeBLL/Entities/TrendLineVeriables.vb
@@ -0,0 +1,5 @@
+﻿Public Class TrendLineVeriables
+    Public M As Decimal
+    Public C As Decimal
+    Public X As Decimal
+End Class
--- Algo2TradeBLL/Indicators/FractalBandsTrendLine.vb
+++ Algo2TradeBLL/Indicators/FractalBandsTrendLine.vb
@@ -1,12 +1,31 @@
 ﻿Namespace Indicator
     Public Module FractalBandsTrendLine
-        Public Sub CalculateFractalBandsTrendLine(ByVal inputPayload As Dictionary(Of Date, Payload), ByRef outputHighPayload As Dictionary(Of Date, Decimal), ByRef outputLowPayload As Dictionary(Of Date, Decimal))
+        Public Sub CalculateFractalBandsTrendLine(ByVal inputPayload As Dictionary(Of Date, Payload), ByRef outputHighPayload As Dictionary(Of Date, TrendLineVeriables), ByRef outputLowPayload As Dictionary(Of Date, TrendLineVeriables))
             If inputPayload IsNot Nothing AndAlso inputPayload.Count > 0 Then
                 Dim fractalHighPayload As Dictionary(Of Date, Decimal) = Nothing
                 Dim fractalLowPayload As Dictionary(Of Date, Decimal) = Nothing
                 Indicator.FractalBands.CalculateFractal(inputPayload, fractalHighPayload, fractalLowPayload)
                 For Each runningPayload In inputPayload
-                    Dim firstUCandle As Payload = GetFractalUFormingCandle(inputPayload, fractalLowPayload, runningPayload.Key, -1)
+                    Dim lastHighUCandle As Payload = GetFractalUFormingCandle(inputPayload, fractalHighPayload, runningPayload.Key, 1)
+                    If lastHighUCandle IsNot Nothing Then
+                        Dim firstHighUCandle As Payload = lastHighUCandle
+                        While firstHighUCandle.High <= lastHighUCandle.High
+                            firstHighUCandle = GetFractalUFormingCandle(inputPayload, fractalHighPayload, firstHighUCandle.PayloadDate, 1)
+                            If firstHighUCandle Is Nothing Then Exit While
+                        End While
+                        If firstHighUCandle IsNot Nothing Then
+                            Dim x1 As Decimal = 0
+                            Dim y1 As Decimal = firstHighUCandle.High
+                            Dim x2 As Decimal = inputPayload.Where(Function(x)
+                                                                       Return x.Key > firstHighUCandle.PayloadDate AndAlso x.Key <= lastHighUCandle.PayloadDate
+                                                                   End Function).Count
+                            Dim y2 As Decimal = lastHighUCandle.High
+
+                            Dim m As Decimal = (y2 - y1) / (x2 - x1)
+                            Dim c As Decimal = y1
+
+                        End If
+                    End If
                 Next
             End If
         End Sub
